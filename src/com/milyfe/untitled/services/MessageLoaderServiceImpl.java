b7cb83c723f8f772f1d3af54ddc07155a0c5f2ce
Brian <bpmill86@gmail.com>
2013-11-12 20:30:04 -0500

    working app

--- src/com/milyfe/untitled/services/MessageLoaderServiceImpl.java
+++ src/com/milyfe/untitled/services/MessageLoaderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.milyfe.untitled.services;
 
 import com.milyfe.untitled.model.Message;
-import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +16,19 @@ public class MessageLoaderServiceImpl implements MessageLoaderService {
 
     private FacebookService facebookService;
     private TwitterService twitterService;
-    private Iterable<MessageService> messageServices;
+    private List<MessageService> messageServices;
 
     public MessageLoaderServiceImpl(FacebookService facebookService,TwitterService twitterService) {
         this.facebookService = facebookService;
         this.twitterService = twitterService;
-        messageServices = Lists.newArrayList(facebookService, twitterService);
+        messageServices = new ArrayList<MessageService>();
+        messageServices.add(facebookService);
+        messageServices.add(twitterService);
     }
 
     @Override
     public List<Message> loadMessagesFromAccessableServices() {
-        List<Message> messages = new ArrayList<>();
+        List<Message> messages = new ArrayList<Message>();
         int days = 30;
         for(MessageService messageService : messageServices) {
            if(messageService.hasAuthentication())  {
